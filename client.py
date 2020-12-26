7e1006237e0abe1380cc3e4f8ac7e646fc0d375b
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2020-12-26 04:05:12 +0200

    gRPC working chat template was made.

--- client.py
+++ client.py
@@ -1,22 +1,44 @@
+import threading
+
 import grpc
 
-import chat_pb2
-import chat_pb2_grpc
+import serverchat_pb2 as schat_pb2
+import serverchat_pb2_grpc as schat_pb2_grpc
 
+
+def message_listener(stub):
+    for action in stub.get_chat_stream(schat_pb2.Empty()):
+        if action.action_type == schat_pb2.Action.ActionType.CONNECT:
+            print('User {} is connected to the server'.format(action.username))
+        elif action.action_type == schat_pb2.Action.ActionType.DISCONNECT:
+            print('User {} is disconnected from the server'.format(action.username))
+        elif action.action_type == schat_pb2.Action.ActionType.SEND_MESSAGE:
+            print('{} > {}'.format(action.username, action.playload))
+
+
+username = input('Enter username:')
 
 # channel = grpc.insecure_channel()
 with grpc.insecure_channel('localhost:50051') as channel:
-    stub = chat_pb2_grpc.ChatStub(channel)            
+    stub = schat_pb2_grpc.ServerChatStub(channel)            
+    
+    threading.Thread(target=message_listener, args=(stub,)).start()
+    
     conn_resp = stub.connect(
-        chat_pb2.ConnectionRequest(
-            username='Alex'))
+        schat_pb2.ConnectionRequest(
+            username=username))
     token = conn_resp.user_token
-    status = stub.send_message(
-        chat_pb2.Message(
-            user_token=token,
-            text='Hello world'))
-    print('Message status', status.is_ok)
-    status = stub.disconnect(
-        chat_pb2.DisconnectionRequest(
-            user_token=token))
-    print('Disconnect status', status.is_ok)
+
+    try:
+        while True:
+            text = input('< ')
+            status = stub.send_message(
+            schat_pb2.Message(
+                user_token=token,
+                text=text))
+            print('Message status', status.is_ok)
+    except KeyboardInterrupt:
+        status = stub.disconnect(
+            schat_pb2.DisconnectionRequest(
+                user_token=token))
+    # print('Disconnect status', status.is_ok)
--- server.py
+++ server.py
@@ -1,65 +1,120 @@
 from concurrent import futures
+import threading
 import uuid
 
 import grpc
+from loguru import logger
 
-import chat_pb2
-import chat_pb2_grpc
+import serverchat_pb2 as schat_pb2
+import serverchat_pb2_grpc as schat_pb2_grpc
 
-tokens = dict()
 
-class ChatServicer(chat_pb2_grpc.ChatServicer):
+class ChatServicer(schat_pb2_grpc.ServerChatServicer):
+
+    def __init__(self):
+        self.tokens = dict()
+        self.tokens_lock = threading.Lock()
+        self.actions = list()
+        self.actions_lock = threading.Lock()
+
+    def add_new_action(self, username, action_type, playload=''):
+        with self.actions_lock:
+            self.actions.append(
+                schat_pb2.Action(
+                    username=username,
+                    action_type=action_type,
+                    playload=playload))
 
     def connect(self, request, context):
-        global tokens
-        if request.username not in tokens.values():
-            token = uuid.uuid4().hex
-            tokens[token] = request.username
-            user_token = token
-            is_ok = True
-            error_message = ''
-        else:
-            user_token = ''
-            is_ok = False
-            error_message = 'User with such name already exists.'
+        with self.tokens_lock:
+            if request.username not in self.tokens.values():
+                # add connect action to list of acctions 
+                self.add_new_action(
+                    request.username,
+                    schat_pb2.Action.ActionType.CONNECT)
+
+                # create and save related token
+                token = uuid.uuid4().hex
+                self.tokens[token] = request.username
+
+                user_token = token
+                is_ok, error_message = True, ''
+
+                logger.debug(f'User {request.username} connected')
+            else:
+                user_token = ''
+                is_ok, error_message = False, 'User with such name already exists.'
+                logger.debug(f'User {request.username} tried unsuccessfully to connect')
+
         
-        return chat_pb2.ConnectionResponse(
+        return schat_pb2.ConnectionResponse(
             user_token=user_token,
-            status=chat_pb2.Status(
+            status=schat_pb2.Status(
                 is_ok=is_ok,
                 error_message=error_message))
          
     def disconnect(self, request, context):
-        global tokens
-        if request.user_token in tokens:
-            del tokens[request.user_token]
+        with self.tokens_lock:
+            if request.user_token in self.tokens:
+                username = self.tokens[request.user_token]
+                # add disconnect action
+                self.add_new_action(
+                    username,
+                    schat_pb2.Action.ActionType.DISCONNECT)
 
-            is_ok, error_message = True, ''
-        else:
-            is_ok, error_message = False, 'There aren\'t such user.'
+                del self.tokens[request.user_token]
 
-        return chat_pb2.Status(
+                is_ok, error_message = True, ''
+
+                logger.debug(f'User {username} disconnected')
+            else:
+                is_ok, error_message = False, 'There aren\'t such user.'
+                logger.debug(f'User {request.username} tried unsuccessfully to disconnected')
+
+
+        return schat_pb2.Status(
             is_ok=is_ok,
             error_message=error_message)
 
     def send_message(self, request, context):
-        global tokens
-        username = tokens.get(request.user_token, None)
-        
-        if username is not None:
-            print('{}> {}'.format(username, request.text))
-            is_ok, error_message = True, ''
-        else:
-            is_ok, error_message = False, 'Token is not valid.'
+        with self.tokens_lock:
+            username = self.tokens.get(request.user_token, None)
+            
+            if username is not None:
+                # add send message action
+                self.add_new_action(
+                    username,
+                    schat_pb2.Action.ActionType.SEND_MESSAGE,
+                    request.text)
 
-        return chat_pb2.Status(
+                print('{}> {}'.format(username, request.text))
+                is_ok, error_message = True, ''
+                logger.debug(f'User {username} sent message {request.text}')
+            else:
+                is_ok, error_message = False, 'Token is not valid.'
+                logger.debug(f'User with token {request.token} tried unsuccessfully to sent message {request.text}')
+
+
+        return schat_pb2.Status(
             is_ok=is_ok,
             error_message=error_message)
+
+    def get_chat_stream(self, request, context):
+        next_action_ind = 0
+        # yield all actions
+        while True:
+            # get actions to send
+            with self.actions_lock:
+                actions_to_send = self.actions[next_action_ind:]
+                next_action_ind = len(self.actions)
+            # send actions
+            for action in actions_to_send:
+                yield action
     
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    chat_pb2_grpc.add_ChatServicer_to_server(
+    schat_pb2_grpc.add_ServerChatServicer_to_server(
         ChatServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
